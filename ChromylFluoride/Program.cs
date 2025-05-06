07b03e29ad276287d44bcc5fdb6afbf553bf8e1a
Satindra Ramalingam <75828216+satindra-r@users.noreply.github.com>
2025-05-06 21:42:34 +0530

    Initial Commit

---
+++ ChromylFluoride/Program.cs
@@ -0,0 +1,228 @@
+﻿using System.Runtime.InteropServices;
+using ComputeSharp;
+using NAudio.Wave;
+
+namespace ChromylFluoride;
+
+public static partial class Program {
+	private const int SrcCopy = 0x00CC0020;
+	private const int BiRgb = 0;
+
+	#region Structs
+
+	[StructLayout(LayoutKind.Sequential)]
+	private struct BitmapInfoHeader {
+		public uint biSize;
+		public int biWidth;
+		public int biHeight;
+		public short biPlanes;
+		public short biBitCount;
+		public uint biCompression;
+		public uint biSizeImage;
+		public int biXPelsPerMeter;
+		public int biYPelsPerMeter;
+		public uint biClrUsed;
+		public uint biClrImportant;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	private struct BitMapInfo {
+		public BitmapInfoHeader bmiHeader;
+
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 1024)]
+		public uint[] bmiColors;
+	}
+
+	#endregion
+
+	#region Imports
+
+	[LibraryImport("user32.dll")]
+	private static partial IntPtr GetDC(IntPtr hWnd);
+
+	[LibraryImport("user32.dll")]
+	private static partial int ReleaseDC(IntPtr hWnd, IntPtr hDc);
+
+	[LibraryImport("gdi32.dll")]
+	private static partial IntPtr CreateCompatibleDC(IntPtr hdc);
+
+	[DllImport("gdi32.dll")]
+	private static extern unsafe IntPtr CreateDIBSection(IntPtr hdc, ref BitMapInfo bmi, uint usage, out uint* bits,
+		IntPtr hSection,
+		uint offset);
+
+	[LibraryImport("gdi32.dll")]
+	private static partial void SelectObject(IntPtr hdc, IntPtr obj);
+
+	[DllImport("gdi32.dll")]
+	private static extern bool BitBlt(IntPtr destDc, int x, int y, int w, int h, IntPtr srcDc, int sx, int sy, int rop);
+
+	[DllImport("gdi32.dll")]
+	private static extern bool DeleteObject(IntPtr obj);
+
+	[DllImport("gdi32.dll")]
+	private static extern bool DeleteDC(IntPtr hdc);
+
+	[LibraryImport("user32.dll")]
+	private static partial short GetAsyncKeyState(int vKey);
+
+	[LibraryImport("gdi32.dll")]
+	private static partial int GetDeviceCaps(IntPtr hdc, int index);
+
+	[DllImport("user32.dll")]
+	private static extern bool SetProcessDPIAware();
+
+	#endregion
+
+	private static int _screenWidth, _screenHeight, _totalPixels;
+	private static bool _running = true;
+
+	private static void GetScreenSize() {
+		var hdc = GetDC(IntPtr.Zero);
+		_screenWidth = GetDeviceCaps(hdc, 118);
+		_screenHeight = GetDeviceCaps(hdc, 117);
+		_totalPixels = _screenWidth * _screenHeight;
+		_ = ReleaseDC(IntPtr.Zero, hdc);
+	}
+
+
+	private static unsafe void ApplyEffect(IntPtr hdcScreen, IntPtr hdcMem, uint* bits, int rand, int timeElapsed) {
+		timeElapsed %= 50;
+		if (timeElapsed >= 40) {
+			return;
+		}
+		
+		
+
+		BitBlt(hdcMem, 0, 0, _screenWidth, _screenHeight, hdcScreen, 0, 0, SrcCopy);
+		using var bufferRead = GraphicsDevice.GetDefault()
+			.AllocateReadOnlyBuffer<uint>(new Span<uint>(bits, _totalPixels));
+		using var bufferWrite = GraphicsDevice.GetDefault()
+			.AllocateReadWriteBuffer<uint>(new Span<uint>(bits, _totalPixels));
+
+		if (timeElapsed < 10) {
+			GraphicsDevice.GetDefault().For(bufferRead.Length,
+				new LsbCorrupt(bufferRead, bufferWrite, _screenWidth, _screenHeight, rand));
+		}
+		else if (timeElapsed < 20) {
+			GraphicsDevice.GetDefault().For(bufferRead.Length,
+				new HueShift(bufferRead, bufferWrite, _screenWidth, _screenHeight, rand));
+		}
+		else if (timeElapsed < 30) {
+			GraphicsDevice.GetDefault().For(bufferRead.Length,
+				new PixelWalk(bufferRead, bufferWrite, _screenWidth, _screenHeight, rand));
+		}
+		else {
+			GraphicsDevice.GetDefault().For(bufferRead.Length,
+				new Gaussian(bufferRead, bufferWrite, _screenWidth, _screenHeight, rand));
+		}
+
+		bufferWrite.CopyTo(new Span<uint>(bits, _totalPixels));
+		BitBlt(hdcScreen, 0, 0, _screenWidth, _screenHeight, hdcMem, 0, 0, SrcCopy);
+	}
+
+	private static unsafe void GenVisuals() {
+		var startTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+		var hdcScreen = GetDC(IntPtr.Zero);
+		var hdcMem = CreateCompatibleDC(hdcScreen);
+		var randGen = new Random();
+		var bmi = new BitMapInfo {
+			bmiHeader = new BitmapInfoHeader {
+				biSize = (uint)Marshal.SizeOf<BitmapInfoHeader>(),
+				biWidth = _screenWidth,
+				biHeight = -_screenHeight,
+				biPlanes = 1,
+				biBitCount = 32,
+				biCompression = BiRgb
+			},
+			bmiColors = new uint[1024]
+		};
+
+		var hBitmap = CreateDIBSection(hdcScreen, ref bmi, 0, out var bits, IntPtr.Zero, 0);
+		SelectObject(hdcMem, hBitmap);
+		while (_running) {
+			ApplyEffect(hdcScreen, hdcMem, bits, randGen.Next(),
+				(int)(DateTimeOffset.UtcNow.ToUnixTimeSeconds() - startTime));
+		}
+
+		DeleteObject(hBitmap);
+		DeleteDC(hdcMem);
+		_ = ReleaseDC(IntPtr.Zero, hdcScreen);
+	}
+
+	private static void GenAudio() {
+		var startTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+		var randGen = new Random();
+		using var waveOut = new WaveOutEvent();
+		while (_running) {
+			if ((DateTimeOffset.UtcNow.ToUnixTimeSeconds() - startTime) % 50 < 40) {
+				var sineWaveGenerator = new WaveProvider32 {
+					Frequency = 220 * (float)Math.Pow(2, (float)randGen.Next(36) / 12),
+					Amplitude = 0.25f,
+					TotalSamples = 441 * 20
+				};
+				waveOut.Init(sineWaveGenerator);
+				waveOut.Play();
+				Thread.Sleep(200);
+				waveOut.Stop();
+			}
+			Thread.Sleep(500);
+		}
+	}
+
+	private static void CheckExit() {
+		while (_running) {
+			if ((GetAsyncKeyState(0x1B) & 0x8000) != 0) // VK_ESCAPE
+			{
+				_running = false;
+				break;
+			}
+
+			Thread.Sleep(50);
+		}
+	}
+
+	private static void Main() {
+		SetProcessDPIAware();
+		GetScreenSize();
+
+		var visualThread = new Thread(GenVisuals);
+		var audioThread = new Thread(GenAudio);
+		var exitThread = new Thread(CheckExit);
+
+		visualThread.Start();
+		audioThread.Start();
+		exitThread.Start();
+
+		visualThread.Join();
+		audioThread.Join();
+		exitThread.Join();
+	}
+}
+
+internal class WaveProvider32 : ISampleProvider {
+	public float Frequency { get; init; }
+	public float Amplitude { get; init; }
+	public int TotalSamples { get; init; }
+	private float _phase;
+	private int _samplesUsed;
+
+	public WaveFormat WaveFormat => WaveFormat.CreateIeeeFloatWaveFormat(44100, 1);
+
+	public int Read(float[] buffer, int offset, int count) {
+		for (var n = 0; n < count; n++) {
+			buffer[offset + n] = 0;
+			for (var i = 0; i < 10; i++) {
+				buffer[offset + n] +=
+					(float)(Math.Exp(-5.0 * _samplesUsed / TotalSamples) * Math.Pow(i + 1, -2) * Amplitude *
+					        Math.Sin((i + 1) * _phase * 2 * Math.PI));
+			}
+
+			_samplesUsed++;
+			_phase += Frequency / WaveFormat.SampleRate;
+			if (_phase > 1) _phase -= 1;
+		}
+
+		return count;
+	}
+}
---
+++ ChromylFluoride/Shader.cs
@@ -0,0 +1,190 @@
+﻿using ComputeSharp;
+
+namespace ChromylFluoride;
+
+[AutoConstructor]
+public readonly partial struct HueShift : IComputeShader {
+	private readonly ReadOnlyBuffer<uint> _bufferRead;
+	private readonly ReadWriteBuffer<uint> _bufferWrite;
+	private readonly int _screenWidth;
+	private readonly int _screenHeight;
+	private readonly int _seed;
+
+	private static int3 GetRgb(uint pixel) {
+		return new int3((int)(pixel & 0x00ff0000) >> 16, (int)(pixel & 0x0000ff00) >> 8, (int)(pixel & 0x000000ff));
+	}
+
+	private static uint ToRgb(int r, int g, int b) {
+		return 0xff000000 | (uint)r << 16 | (uint)g << 8 | (uint)b;
+	}
+
+	public void Execute() {
+		var rgb = GetRgb(_bufferRead[ThreadIds.X]);
+		var inc = 0;
+		var range = Hlsl.Max(Hlsl.Max(rgb.R, rgb.G), rgb.B) - Hlsl.Min(Hlsl.Min(rgb.R, rgb.G), rgb.B);
+		if (_seed % 256 < range) {
+			inc = 15;
+		}
+
+		if (rgb.R > rgb.G && rgb.R >= rgb.B) {
+			if (rgb.G >= rgb.B) {
+				rgb.G += inc;
+				rgb.G = Math.Min(rgb.R, rgb.G);
+			}
+			else {
+				rgb.B -= inc;
+				rgb.B = Math.Max(rgb.G, rgb.B);
+			}
+		}
+		else if (rgb.G > rgb.B && rgb.G >= rgb.R) {
+			if (rgb.B >= rgb.R) {
+				rgb.B += inc;
+				rgb.B = Math.Min(rgb.G, rgb.B);
+			}
+			else {
+				rgb.R -= inc;
+				rgb.R = Math.Max(rgb.B, rgb.R);
+			}
+		}
+		else if (rgb.B > rgb.R && rgb.B >= rgb.G) {
+			if (rgb.R >= rgb.G) {
+				rgb.R += inc;
+				rgb.R = Math.Min(rgb.B, rgb.R);
+			}
+			else {
+				rgb.G -= inc;
+				rgb.G = Math.Max(rgb.R, rgb.G);
+			}
+		}
+
+		_bufferWrite[ThreadIds.X] = ToRgb(rgb.R, rgb.G, rgb.B);
+	}
+}
+
+[AutoConstructor]
+public readonly partial struct LsbCorrupt : IComputeShader {
+	private readonly ReadOnlyBuffer<uint> _bufferRead;
+	private readonly ReadWriteBuffer<uint> _bufferWrite;
+	private readonly int _screenWidth;
+	private readonly int _screenHeight;
+	private readonly int _seed;
+
+	private static uint GenRand(uint seed) {
+		seed ^= seed << 13;
+		seed ^= seed >> 17;
+		seed ^= seed << 5;
+		return seed;
+	}
+
+	public void Execute() {
+		var rand = GenRand((uint)(_seed * ThreadIds.X + 1));
+		_bufferWrite[ThreadIds.X] = (_bufferRead[ThreadIds.X] & 0xffe0e0e0) | (rand & 0x001f1f1f);
+	}
+}
+
+[AutoConstructor]
+public readonly partial struct PixelWalk : IComputeShader {
+	private readonly ReadOnlyBuffer<uint> _bufferRead;
+	private readonly ReadWriteBuffer<uint> _bufferWrite;
+	private readonly int _screenWidth;
+	private readonly int _screenHeight;
+	private readonly int _seed;
+
+	private static uint GenRand(uint seed) {
+		seed ^= seed << 13;
+		seed ^= seed >> 17;
+		seed ^= seed << 5;
+		return seed;
+	}
+
+	public void Execute() {
+		var x = ThreadIds.X % _screenWidth;
+		var y = ThreadIds.X / _screenWidth;
+
+		var rand = Hlsl.Abs((int)GenRand((uint)(_seed * ThreadIds.X + 1)));
+		var shiftX = (rand & 1) == 1;
+		var shiftY = (rand & 2) == 2;
+		var posX = x;
+		var posY = y;
+
+		rand >>= 2;
+		if (shiftX) {
+			var dx = rand % 3 - 1;
+			posX = x + dx;
+		}
+
+		if (shiftY) {
+			var dy = rand / 3 % 3 - 1;
+			posY = y + dy;
+		}
+
+		if (posX < 0) {
+			posX += _screenWidth;
+		}
+
+		if (posX > _screenWidth - 1) {
+			posX -= _screenWidth;
+		}
+
+		if (posY < 0) {
+			posY += _screenHeight;
+		}
+
+		if (posY > _screenHeight - 1) {
+			posY -= _screenHeight;
+		}
+
+		_bufferWrite[ThreadIds.X] = _bufferRead[posY * _screenWidth + posX];
+	}
+}
+
+[AutoConstructor]
+public readonly partial struct Gaussian : IComputeShader {
+	private readonly ReadOnlyBuffer<uint> _bufferRead;
+	private readonly ReadWriteBuffer<uint> _bufferWrite;
+	private readonly int _screenWidth;
+	private readonly int _screenHeight;
+	private readonly int _seed;
+
+	private static int3 GetRgb(uint pixel) {
+		return new int3((int)(pixel & 0x00ff0000) >> 16, (int)(pixel & 0x0000ff00) >> 8, (int)(pixel & 0x000000ff));
+	}
+
+	private static uint ToRgb(int r, int g, int b) {
+		return 0xff000000 | (uint)r << 16 | (uint)g << 8 | (uint)b;
+	}
+
+	public void Execute() {
+		var kernel = new float3x3(
+			0.0625f, 0.125f, 0.0625f,
+			0.125f, 0.25f, 0.125f,
+			0.0625f, 0.125f, 0.0625f
+		);
+		var x = ThreadIds.X % _screenWidth;
+		var y = ThreadIds.X / _screenWidth;
+		var xPrev = Hlsl.Max(x - 1, 0);
+		var yPrev = Hlsl.Max(y - 1, 0);
+		var xNext = Hlsl.Min(x + 1, _screenWidth - 1);
+		var yNext = Hlsl.Min(y + 1, _screenHeight - 1);
+		var xPos = new int3(xPrev, x, xNext);
+		var yPos = new int3(yPrev, y, yNext);
+
+		float sumR = 0;
+		float sumG = 0;
+		float sumB = 0;
+		for (var dx = 0; dx < 3; dx++) {
+			for (var dy = 0; dy < 3; dy++) {
+				var rgb = GetRgb(_bufferRead[yPos[dy] * _screenWidth + xPos[dx]]);
+				sumR += kernel[dy][dx] * rgb.R;
+				sumG += kernel[dy][dx] * rgb.G;
+				sumB += kernel[dy][dx] * rgb.B;
+			}
+		}
+
+		var sumClampedR = (int)Hlsl.Round(Hlsl.Clamp(sumR, 0.0f, 255.0f));
+		var sumClampedG = (int)Hlsl.Round(Hlsl.Clamp(sumG, 0.0f, 255.0f));
+		var sumClampedB = (int)Hlsl.Round(Hlsl.Clamp(sumB, 0.0f, 255.0f));
+
+		_bufferWrite[ThreadIds.X] = ToRgb(sumClampedR, sumClampedG, sumClampedB);
+	}
+}
